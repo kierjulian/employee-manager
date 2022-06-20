1ba858c4a468f7f1ebbe5f2a5b13946186b5be31
Kier Julian Genoguin <kierj.genoguin@gmail.com>
2022-06-20 12:09:35 +0800

    Added employee model and mysql configuration.
    Signed-off-by: Kier Julian Genoguin <kierj.genoguin@gmail.com>

---
+++ src/main/java/ph/edu/up/employeemanager/model/Employee.java
@@ -0,0 +1,95 @@
+package ph.edu.up.employeemanager.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+@Entity
+public class Employee implements Serializable {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO,
+					generator = "employee_sequence"
+	)
+	@SequenceGenerator(sequenceName = "employee_sequence",
+						name = "employee_sequence",
+						allocationSize = 1
+	)
+	@Column(nullable = false,
+			updatable = false
+	)
+	private Long id;
+	private String name;
+	private String email;
+	private String jobTitle;
+	private String phoneNumber;
+	private String imageUrl;
+	
+	@Column(nullable = false,
+			updatable = false
+	)
+	private String employeeCode;
+	
+	public Long getId() {
+		return id;
+	}
+	
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+	
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	public String getJobTitle() {
+		return jobTitle;
+	}
+	
+	public void setJobTitle(String jobTitle) {
+		this.jobTitle = jobTitle;
+	}
+	
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+	
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+	
+	public String getImageUrl() {
+		return imageUrl;
+	}
+	
+	public void setImageUrl(String imageUrl) {
+		this.imageUrl = imageUrl;
+	}
+
+	public String getEmployeeCode() {
+		return employeeCode;
+	}
+
+	public void setEmployeeCode(String employeeCode) {
+		this.employeeCode = employeeCode;
+	}
+
+}
